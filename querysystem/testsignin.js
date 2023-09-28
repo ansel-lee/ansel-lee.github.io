acb31dcab3c2aa08bdd0dad67c0af5fc4fc0c5b1
Ansel Lee <ansellee23@gmail.com>
2023-09-29 00:01:53 +0800

    update files
    added new features such as printing, revealing password, using a password field and more code comments
    Apps script and sheet code is 3.4

--- querysystem/testsignin.js
+++ querysystem/testsignin.js
@@ -4,8 +4,8 @@
 
 
 function globalVars() {
-    window.scriptlink = "https://script.google.com/macros/s/AKfycbyMSm9vQWsl6qoCVNvIUpjbLj3PF1ReQ4gt3Ba2rAEEjxOyByABUmI6jQO96ZhpWDHd/exec";
-    window.verNum = 3.2;
+    window.scriptlink = "https://script.google.com/macros/s/AKfycbw_7znCWq6wvusF-wUiSljt0xPvl1CQuVWlhpZciRjq-dB8ubsdNDH8VSIaZu3vqB0/exec";
+    window.verNum = 3.4;
     window.googleAuthID = "1058554454512-daeil9rg9nahtovai8mji510fq2fi8qg.apps.googleusercontent.com";
     window.formType = ["Google", "userID", "userPass"]
     // what fields are shown.
@@ -24,7 +24,8 @@ function changeDarkLight() {
 }
 
 
-
+// uses google federated sign-in. However, there is no CSRF double cookie check on server side
+// as google apps script does not allow cookies 
 function googleSignIn(callbackInfo) {
     if (callbackInfo.clientId === callbackInfo.client_id && callbackInfo.clientId === googleAuthID) {
         callAppsScript("Google/JWT", callbackInfo.credential);
@@ -32,24 +33,23 @@ function googleSignIn(callbackInfo) {
         console.log("Wrong client ID");
     }
 
-    /* example object {
-        "clientId": "1058554454512-daeil9rg9nahtovai8mji510fq2fi8qg.apps.googleusercontent.com",
+    /* example object {"clientId": "1058554454512-daeil9rg9nahtovai8mji510fq2fi8qg.apps.googleusercontent.com",
         "client_id": "1058554454512-daeil9rg9nahtovai8mji510fq2fi8qg.apps.googleusercontent.com",
-        "credential": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjYwODNkZDU5ODE2NzNmNjYxZmRlOWRhZTY0NmI2ZjAzODBhMDE0NWMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJuYmYiOjE2ODYwMjc5NDIsImF1ZCI6IjEwNTg1NTQ0NTQ1MTItZGFlaWw5cmc5bmFodG92YWk4bWppNTEwZnEyZmk4cWcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDc4NjU1NzYwMTk1MTY1MTI2MDUiLCJlbWFpbCI6ImFuc2VsbGVlMjNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF6cCI6IjEwNTg1NTQ0NTQ1MTItZGFlaWw5cmc5bmFodG92YWk4bWppNTEwZnEyZmk4cWcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJuYW1lIjoiQW5zZWwgTGVlIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBY0hUdGRuc3VoOVNIMzFCRmZVVXVmUThGaVM2OFprZ3hzVGtmVzFscmRPPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IkFuc2VsIiwiZmFtaWx5X25hbWUiOiJMZWUiLCJpYXQiOjE2ODYwMjgyNDIsImV4cCI6MTY4NjAzMTg0MiwianRpIjoiNTEzYTQ0YzFhYjcxZmRmNmZkNTE0NDFjZDA2M2RlNjVhMzJiYTg0NCJ9.pPRoOBElAv9wiuOeow948JD0V_GFd5PTOrallCg7h-aKCleL-6d41vrXuAaOKj-nAOCK1l-iew1YjREL8QA4zZiSlWJsTGwgtn_IzGYQt0jtLzGcX0tP2z-vBLnsbZwG4X8j5zrq1HBINKb0A0GfCzvVCE67ZlYpK0SzbH2MS-CAGOPp6inRwDwjLLzh5DNJttdNv9CQA8zbDYPHY7P3tt0MIzoFTrMWI9HesVQanNKrpgEbhcteXrlxpY1mYw3YST0KPMuy-YtDVfDfKJhqQss-llRCqYUwp3QxhQ1VbIHS6jKwn16ZaEFrI8DaJT1jytHUJiXOi8PPvvlZ13bQ7Q",
-        "select_by": "btn"
-    }*/
+        "credential": "", "select_by": "btn"}*/
 }
 
 function checkIdentity(e) {
-    console.log("identity listener called")
-    var userText = document.getElementById("userinfo").value
-    callAppsScript("userID", userText)
+    console.log("identity listener called");
+    var userText = document.getElementById("userinfo").value;
+    callAppsScript("userID", userText);
 }
 
 function checkIdentiPass(e) {
-    var username = document.getElementById("userinfo").value
-    var password = document.getElementById("userPass").value
-    callAppsScript("userPass", [username, password])
+    var username = document.getElementById("userinfo").value;
+    var password = document.getElementById("userPass").value;
+    callAppsScript("userPass", [username, password]);
+    // server side hashing is implemented 
+    // reference: https://security.stackexchange.com/questions/8596/https-security-should-password-be-hashed-server-side-or-client-side
 }
 
 
@@ -93,9 +93,13 @@ function callAppsScript(authType, cred) {
                 handleServerErrs(thisresponse)
             } else {
                 // Request finished. Do processing here.
-                populatelist(thisresponse[2], thisresponse[3])
-                document.getElementById("loadingscreen").classList.add("hidden")
-                document.getElementById("successMsg").classList.remove("hidden")
+                document.getElementById("receivedInfo").innterHTML = "<p>Field</p><p>Details</p><hr />";
+                populatelist(thisresponse[2], thisresponse[3]);
+                document.getElementById("loadingscreen").classList.add("hidden");
+                document.getElementById("successMsg").classList.remove("hidden");
+
+                // reinstate enter event listener upon successful listing
+                document.getElementById("userinfo").addEventListener("keypress", usercheckenter)
             }
         }
     };
@@ -127,10 +131,13 @@ function callAppsScript(authType, cred) {
                 break;
             case "user-not-found-5":
                 document.getElementById("passdiv").classList.remove("hidden");
+                document.getElementById("userPass").focus();
                 document.getElementById("submitBtn").removeEventListener("click", checkIdentity);
                 document.getElementById("submitBtn").addEventListener("click", checkIdentiPass);
                 document.getElementById("errors").classList.add("hidden");
-                document.getElementById("loadingscreen").classList.add("hidden")
+                document.getElementById("loadingscreen").classList.add("hidden");
+
+                document.getElementById("userinfo").removeEventListener("keypress", usercheckenter);
                 break;
             case "missing-sheet":
                 document.getElementById("wrongVersion").classList.remove("hidden");
@@ -172,7 +179,7 @@ function populatelist(names, val) {
 
                 el = document.createElement("p");
                 var j = 0;
-                var withNewLines = val[i].split(/\r?\n|\r|\n/g);
+                var withNewLines = String(val[i]).split(/\r?\n|\r|\n/g);
                 while (j < withNewLines.length) {
                     if (j !== 0) { el.innerHTML += "<br /><br />" }
                     el.appendChild(document.createTextNode(withNewLines[j]));
@@ -183,6 +190,33 @@ function populatelist(names, val) {
         }
 
         i++
+    }
+}
+
+function usercheckenter(e) {
+    if (e.key == "Enter") {
+        if (e.currentTarget == document.getElementById("userinfo")) {
+            checkIdentity();
+            console.log("checking username via enter press")
+        } else {
+            checkIdentiPass()
+            console.log("checking password by enter press")
+        }
+    }
+}
+
+function printSettings() {
+    document.body.classList.add("lightmode");
+    window.print()
+    document.body.classList.remove("lightmode");
+}
+
+function passRevealEvt() {
+    var e = document.getElementById("passReveal")
+    if (e.matches(":hover") || e.matches(":focus")) {
+        document.getElementById("userPass").type = "text"
+    } else {
+        document.getElementById("userPass").type = "password"
     }
 }
 
@@ -204,14 +238,23 @@ function init() {
         document.getElementById("google-signinbox").classList.add("hidden")
     }
 
+    document.getElementById("userPass").addEventListener("keypress", usercheckenter);
     if (formType.includes("userID")) {
         document.getElementById("identifierPassword").classList.remove("hidden");
         document.getElementById("submitBtn").addEventListener("click", checkIdentity);
-        console.log("identity listener fired")
+        console.log("identity listener fired");
+        document.getElementById("userinfo").addEventListener("keypress", usercheckenter);
     } else if (formType.includes("userPass")) {
-        document.getElementById("identifierPassword").classList.remove("hidden")
-        document.getElementById("passdiv").classList.remove("hidden")
-        document.getElementById("submitBtn").addEventListener("click", checkIdentiPass)
+        document.getElementById("identifierPassword").classList.remove("hidden");
+        document.getElementById("passdiv").classList.remove("hidden");
+        document.getElementById("submitBtn").addEventListener("click", checkIdentiPass);
+        console.log("sending userID and Password in the same packet")
     }
 
+    document.getElementById("passReveal").addEventListener("mouseover", passRevealEvt)
+    document.getElementById("passReveal").addEventListener("mouseout", passRevealEvt)
+    document.getElementById("passReveal").addEventListener("focus", passRevealEvt)
+    document.getElementById("passReveal").addEventListener("focusout", passRevealEvt)
+
+    document.getElementById("print").addEventListener("click", printSettings)
 }
